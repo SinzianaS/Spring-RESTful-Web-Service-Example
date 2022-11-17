b95b395f651be7b6516ff13edb1920b18386e074
Sinziana Sovejanu <27846889+SinzianaS@users.noreply.github.com>
2022-11-17 18:05:03 +0200

    generated project

---
+++ src/test/java/com/restexample/restserviceexample/RestServiceExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.restexample.restserviceexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestServiceExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
